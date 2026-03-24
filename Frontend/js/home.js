82cb097653d381a1cc23623853565d750e7c9983
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-03-24 05:51:35 +0530

    home page ui

---
+++ Frontend/js/home.js
@@ -0,0 +1,8 @@
+// Scroll effect
+$(window).scroll(function(){
+    if($(this).scrollTop() > 50){
+        $('.navbar').addClass('scrolled');
+    } else {
+        $('.navbar').removeClass('scrolled');
+    }
+});
