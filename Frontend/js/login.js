fffb439efd082b3f52b70833c27da80b647ad912
Adeepa_Bimsara <adeepabimsara6@gmail.com>
2026-03-24 06:05:38 +0530

    login page ui

---
+++ Frontend/js/login.js
@@ -0,0 +1,7 @@
+$("#signUp").click(function () {
+    $("#container").addClass("right-panel-active");
+});
+
+$("#signIn").click(function () {
+    $("#container").removeClass("right-panel-active");
+});
